80e16c13771e93808a00e0b8d9377bf3b7fd5c52
dinh.cong.quy <dinh.cong.quy@linecorp.com>
2022-01-13 12:26:36 +0700

    Add auto LGTM

---
+++ README.md
@@ -0,0 +1,10 @@
+# Quick LGTM for lazy people
+
+## Getting Started
+1. Clone the repository
+2. Open extension page in Chrome
+3. Click the `Load Unpacked Extension` button
+
+## How to use
+1. Navigate to the PR page
+2. Click the `LGTM` button
---
+++ content-script.js
@@ -0,0 +1,66 @@
+var button=document.createElement("button"); 
+document.body.appendChild(button);
+button.style.position="fixed";
+button.style.right="50px";
+button.style.bottom="50px";
+button.style.width="100px";
+button.style.height="100px";
+button.style.display="flex";
+button.style.backgroundColor="#34d058";
+button.style.zIndex="9999";
+button.style.borderRadius="50%";
+button.style.boxShadow="0 0 5px black";
+button.style.color="white";
+button.style.justifyContent="center";
+button.style.alignItems="center";
+button.innerText="LGTM";
+
+function handleReview() {
+    const currentUrl = window.location.pathname;
+    if (currentUrl.endsWith("/files")) {
+        addReview();
+        return;
+    }
+
+    const reviewUrl = window.location.origin + currentUrl + "/files#auto-review";
+    window.location.href = reviewUrl;
+}
+
+function validateState() {
+    const stateElement = document.querySelector(".State");
+    if (!stateElement) return false;
+
+    const state = stateElement.innerText;
+    if (state.toLowerCase().trim() === "open") {
+        return true;
+    }
+    return false;
+}
+
+button.addEventListener('click', function() {
+    const isValidState = validateState();
+    if (isValidState) {
+        handleReview();
+    }
+})
+
+function addReview() {
+    const reviewEle = document.getElementById('pull_request_review_body');
+    const approveCheckbox = document.querySelector('input[name="pull_request_review[event]"][value="approve"]');
+    if (!reviewEle || !approveCheckbox) return;
+
+    reviewEle.value = "LGTM";
+    approveCheckbox.click();
+    document.querySelectorAll("button[type='submit']").forEach((btn) => {
+        if (btn.innerHTML.toLowerCase().includes('submit review')) {
+            btn.click();
+        }
+    })
+}
+
+window.addEventListener('load', () => {
+    const currentUrl = window.location.href;
+    if (currentUrl.endsWith("/files#auto-review")) {
+        addReview();
+    }
+})
