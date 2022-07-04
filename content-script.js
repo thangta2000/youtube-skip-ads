eb9bc76e9736f82cd14aaf068fddb8f991c0de0c
Thang Ta <ta.duy.thang@linecorp.com>
2022-07-04 21:01:02 +0700

    First commit

--- content-script.js
+++ content-script.js
@@ -1,77 +1,26 @@
-const button = document.createElement('button');
-document.body.appendChild(button);
-button.style.cssText = `
-    position: fixed;
-    right: 50px;
-    bottom: 50px;
-    z-index: 9999;
-    background-color: rgb(40, 167, 69);
-    color: #fff;
-    padding: 12px 24px;
-    text-align: center;
-    border-radius: 2em;
-    line-height: 20px;
-    font-weight: 500;
-    font-size: 20px;
-    border: none;
-    box-shadow: 2px 3px 3px #e1e3e6;
-`;
-button.innerText = 'LGTM!';
+const skipAds = () => {
+  const btn = document.querySelector(
+    '.ytp-ad-skip-button',
+  )
 
-function addReview() {
-  const reviewEle = document.getElementById('pull_request_review_body');
-  const approveCheckbox = document.querySelector(
-    'input[name="pull_request_review[event]"][value="approve"]',
-  );
-  if (!reviewEle || !approveCheckbox) return;
-
-  reviewEle.value = 'LGTM';
-  approveCheckbox.click();
-  document.querySelectorAll("button[type='submit']").forEach((btn) => {
-    if (btn.innerHTML.toLowerCase().includes('submit review')) {
-      btn.click();
-    }
-  });
+  if (!btn) return
+  btn.click()
 }
-
-function validatePRState() {
-  const stateElement = document.querySelector('.State');
-  if (!stateElement) return false;
-
-  const state = stateElement.innerText;
-  if (state.toLowerCase().trim() === 'open') {
-    return true;
-  }
-  return false;
-}
-
-function handleReview() {
-  const currentUrl = window.location.pathname;
-  if (currentUrl.endsWith('/files')) {
-    addReview();
-    return;
-  }
-
-  const reviewUrl = window.location.origin + currentUrl + '/files#auto-review';
-  window.location.href = reviewUrl;
-}
-
-button.addEventListener('click', function () {
-  const isValidState = validatePRState();
-  if (isValidState) {
-    handleReview();
-  }
-});
 
 window.addEventListener('load', () => {
-  const currentUrl = window.location.href;
-  if (currentUrl.endsWith('/files#auto-review')) {
-    addReview();
+  skipAds()
+  setTimeout(skipAds, 250)
+  setTimeout(skipAds, 500)
+})
+
+let lastUrl = location.href
+new MutationObserver(() => {
+  const url = location.href
+  if (url !== lastUrl) {
+    lastUrl = url
+    setTimeout(skipAds, 250)
+    setTimeout(skipAds, 500)
+    setTimeout(skipAds, 750)
+    setTimeout(skipAds, 1000)
   }
-
-  document.addEventListener('keypress', (e) => {
-    // Ctrl + L
-    if(e.code === 'KeyL' && e.ctrlKey) handleReview();
-  })
-});
-
+}).observe(document, { subtree: true, childList: true })
